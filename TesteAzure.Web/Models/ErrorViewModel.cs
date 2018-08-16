e32d5e349ca560ed1b3d3883111d98eb9ec5b462
Tiago J. Grillo <tijgrillo@gmail.com>
2018-08-16 15:54:17 -0300

    primeiro commit do teste

---
+++ TesteAzure.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TesteAzure.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
